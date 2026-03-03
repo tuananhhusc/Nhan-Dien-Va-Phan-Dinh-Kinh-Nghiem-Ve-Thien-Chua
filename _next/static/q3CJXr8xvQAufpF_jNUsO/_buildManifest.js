40effc19d3800565d507fc514caf19f1524b996a
Tuan Anh <tuananhhusc@example.com>
2026-03-03 12:04:25 +0700

    Updates

---
+++ _next/static/q3CJXr8xvQAufpF_jNUsO/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Nhan-Dien-Va-Phan-Dinh-Kinh-Nghiem-Ve-Thien-Chua//_next/:path+",
+        "destination": "/Nhan-Dien-Va-Phan-Dinh-Kinh-Nghiem-Ve-Thien-Chua/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
